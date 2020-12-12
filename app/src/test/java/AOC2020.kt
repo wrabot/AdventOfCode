b8fb587a8ecf09b00b81e9c75b270862626bbe48
Wilfrid Rabot <wilfird.rabot@gmail.com>
2020-12-12 17:20:15 +0100

    2020 day12

--- app/src/test/java/AOC2020.kt
+++ app/src/test/java/AOC2020.kt
@@ -278,4 +278,75 @@ class AOC2020 : BaseTest("AOC2020") {
     }
 
     private val directions = listOf(1 to 0, 1 to -1, 0 to -1, -1 to -1, -1 to 0, -1 to 1, 0 to 1, 1 to 1)
+
+    @Test
+    fun day12() = test(2) { lines ->
+        day12part1(lines)
+        day12part2(lines)
+    }
+
+    private fun day12part1(lines: List<String>) {
+        var x = 0
+        var y = 0
+        var dir = 0
+        lines.map { Pair(it.first(), it.drop(1).toInt()) }.forEach { (op, value) ->
+            when (op) {
+                'R' -> dir = (dir - value + 360) % 360
+                'L' -> dir = (dir + value) % 360
+                'E' -> x += value
+                'N' -> y -= value
+                'W' -> x -= value
+                'S' -> y += value
+                'F' -> when (dir) {
+                    0 -> x += value
+                    90 -> y -= value
+                    180 -> x -= value
+                    270 -> y += value
+                    else -> error("Should not happen")
+                }
+                else -> error("Should not happen")
+            }
+        }
+        (abs(x) + abs(y)).log()
+    }
+
+    private fun day12part2(lines: List<String>) {
+        var x = 0
+        var y = 0
+        var wx = 10
+        var wy = -1
+        lines.map { Pair(it.first(), it.drop(1).toInt()) }.forEach { (op, value) ->
+            when (op) {
+                'R' -> rotate(wx, wy, 360 - value).let {
+                    wx = it.first
+                    wy = it.second
+                }
+                'L' -> rotate(wx, wy, value).let {
+                    wx = it.first
+                    wy = it.second
+                }
+                'E' -> wx += value
+                'N' -> wy -= value
+                'W' -> wx -= value
+                'S' -> wy += value
+                'F' -> {
+                    x += value * wx
+                    y += value * wy
+                }
+                else -> error("Should not happen")
+            }
+        }
+        (abs(x) + abs(y)).log()
+    }
+
+    private fun rotate(x: Int, y: Int, degree: Int) = when (degree) {
+        90 -> y to -x
+        180 -> -x to -y
+        270 -> -y to x
+        else -> error("Should not happen")
+    }
+
+    @Test
+    fun day13() = test(1) { lines ->
+    }
 }
