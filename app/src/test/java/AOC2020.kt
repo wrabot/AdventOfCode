41ec92418bd3263eb9c4cb1a813e484853590661
Wilfrid Rabot <wilfird.rabot@gmail.com>
2020-12-18 08:09:01 +0100

    2020 day18

--- app/src/test/java/AOC2020.kt
+++ app/src/test/java/AOC2020.kt
@@ -528,4 +528,30 @@ class AOC2020 : BaseTest("AOC2020") {
         }
         return count
     }
+
+    @Test
+    fun day18() = test(2) { lines ->
+        val list = lines.map { it.replace(" ", "") }
+        list.map { Expr(it, false).eval() }.sum().log() //part1
+        list.map { Expr(it, true).eval() }.sum().log() //part2
+    }
+
+    data class Expr(val text: String, val part2: Boolean) {
+        fun eval(): BigInteger {
+            val stack = mutableListOf(operand())
+            while (index < text.length) {
+                when (text[index++]) {
+                    ')' -> break
+                    '*' -> if (part2) stack.add(operand()) else stack[stack.lastIndex] *= operand()
+                    '+' -> stack[stack.lastIndex] += operand()
+                    else -> error("invalid operator")
+                }
+            }
+            return stack.reduce { acc, bi -> acc * bi }
+        }
+
+        private var index: Int = 0
+        private fun operand() = if (text[index++] == '(') eval() else text[index - 1].toString().toBigInteger()
+    }
+
 }
