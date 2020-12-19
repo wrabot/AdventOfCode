004555a1ca218344879dca6107eac42657cd6566
Wilfrid Rabot <wilfird.rabot@gmail.com>
2020-12-19 12:55:28 +0100

    2020 day19

--- app/src/test/java/AOC2020.kt
+++ app/src/test/java/AOC2020.kt
@@ -554,4 +554,33 @@ class AOC2020 : BaseTest("AOC2020") {
         private fun operand() = if (text[index++] == '(') eval() else text[index - 1].toString().toBigInteger()
     }
 
+    @Test
+    fun day19() = test(2) { lines ->
+        val rules = lines.takeWhile { it.isNotBlank() }.map { it.split(": ") }.map { (id, rule) ->
+            id to rule.replace("\"", "").split(" | ").map { it.split(" ") }
+        }.toMap()
+        val messages = lines.takeLastWhile { it.isNotBlank() }
+
+        //part1
+        val regex0 = rules.createRegex("0").toRegex()
+        messages.count { it.matches(regex0) }.log()
+
+        //part2
+        // rule 8 and 11 only used by rule 0
+        // 0: 8 11
+        // 8: 42 | 8
+        // 11: 42 11 31
+        // the rule 0 can be transform as 0: 42 8 31 | 42 0 31
+        val regex42 = rules.createRegex("42")
+        val regex31 = rules.createRegex("31")
+        val regex8 = "$regex42+".toRegex()
+        val regex0Recursive = "$regex42(.*)$regex31".toRegex()
+        messages.count { regex0Recursive.matches(it) && regex8.matches(it.recursiveMatches(regex0Recursive)) }.log()
+    }
+
+    private fun CharSequence.recursiveMatches(regex: Regex): CharSequence =
+        regex.matchEntire(this)?.groupValues?.getOrNull(1)?.recursiveMatches(regex) ?: this
+
+    private fun Map<String, List<List<String>>>.createRegex(id: String): String =
+        this[id]?.joinToString("|", "(?:", ")") { rule -> rule.joinToString("") { createRegex(it) } } ?: id
 }
