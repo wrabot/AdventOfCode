2744aaaf654d21fb153ec32df0fe69753319be3c
Wilfrid Rabot <wilfird.rabot@gmail.com>
2020-12-19 13:13:17 +0100

    2015 day6

--- app/src/test/java/AOC2015.kt
+++ app/src/test/java/AOC2015.kt
@@ -1,5 +1,6 @@
 import org.junit.Test
 import java.security.MessageDigest
+import java.util.*
 
 
 class AOC2015 : BaseTest("AOC2015") {
@@ -106,5 +107,51 @@ class AOC2015 : BaseTest("AOC2015") {
             }
             ok
         }.log()
+    }
+
+    @Test
+    fun day6() = test(1) { lines ->
+        val orders = lines.map { it.removePrefix("turn ").split(" ") }.map { (type, tl, _, br) ->
+            LightOrder(LightOrder.Type.valueOf(type), tl.toIntPair(), br.toIntPair())
+        }
+        val size = 1000
+
+        //part1
+        val binaryLights = BitSet(size * size)
+        orders.forEach {
+            for (i in it.tl.first..it.br.first) {
+                for (j in it.tl.second..it.br.second) {
+                    val index = i * size + j
+                    when (it.type) {
+                        LightOrder.Type.toggle -> binaryLights.flip(index)
+                        LightOrder.Type.on -> binaryLights.set(index)
+                        LightOrder.Type.off -> binaryLights.clear(index)
+                    }
+                }
+            }
+        }
+        binaryLights.cardinality().log()
+
+        //part2
+        val lights = Array<Int>(size * size) { 0 }
+        orders.forEach {
+            for (i in it.tl.first..it.br.first) {
+                for (j in it.tl.second..it.br.second) {
+                    val index = i * size + j
+                    when (it.type) {
+                        LightOrder.Type.toggle -> lights[index] += 2
+                        LightOrder.Type.on -> lights[index]++
+                        LightOrder.Type.off -> if (lights[index] > 0) lights[index]--
+                    }
+                }
+            }
+        }
+        lights.sum().log()
+    }
+
+    private fun String.toIntPair() = split(",").toInts().zipWithNext().first()
+
+    data class LightOrder(val type: Type, val tl: Pair<Int, Int>, val br: Pair<Int, Int>) {
+        enum class Type { toggle, on, off }
     }
 }
