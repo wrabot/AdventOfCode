424a953ab7c3338303c4b10a5dbe6458fc446ba5
Wilfrid Rabot <wilfird.rabot@gmail.com>
2020-12-19 18:13:59 +0100

    2015 day7

--- app/src/test/java/AOC2015.kt
+++ app/src/test/java/AOC2015.kt
@@ -154,4 +154,40 @@ class AOC2015 : BaseTest("AOC2015") {
     data class LightOrder(val type: Type, val tl: Pair<Int, Int>, val br: Pair<Int, Int>) {
         enum class Type { toggle, on, off }
     }
+
+    @Test
+    fun day7() = test(1) { lines ->
+        val circuit = Circuit()
+        lines.map { it.split(" -> ") }.forEach { (input, output) ->
+            circuit.elements[output] = input.split(" ").let { operation ->
+                Circuit.Element(when (operation.size) {
+                    1 -> { -> circuit.operand(operation[0]) } // 123
+                    2 -> { -> circuit.operand(operation[1]).inv() } // not a -> b
+                    3 -> when (operation[1]) {
+                        "AND" -> { -> (circuit.operand(operation[0]) and circuit.operand(operation[2])) }
+                        "OR" -> { -> (circuit.operand(operation[0]) or circuit.operand(operation[2])) }
+                        "LSHIFT" -> { -> (circuit.operand(operation[0]) shl circuit.operand(operation[2])) }
+                        "RSHIFT" -> { -> (circuit.operand(operation[0]) shr circuit.operand(operation[2])) }
+                        else -> error("invalid operation")
+                    }
+                    else -> error("invalid size")
+                })
+            }
+        }
+        val a = circuit.operand("a").log()
+        circuit.elements.forEach { it.value.cache = null }
+        circuit.elements.getValue("b").cache = a
+        circuit.operand("a").log()
+    }
+
+
+    class Circuit {
+        data class Element(val eval: () -> Int) {
+            var cache: Int? = null
+            val value get() = cache ?: eval().apply { cache = this and 0xFFFF }
+        }
+
+        val elements = mutableMapOf<String, Element>()
+        fun operand(input: String) = input.toIntOrNull() ?: elements.getValue(input).value
+    }
 }
