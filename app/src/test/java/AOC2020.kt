089e6edae9c2438a5be0cc216621e2165b9c80ae
Wilfrid Rabot <wilfird.rabot@gmail.com>
2020-12-22 08:08:24 +0100

    2020 day22

--- app/src/test/java/AOC2020.kt
+++ app/src/test/java/AOC2020.kt
@@ -686,4 +686,56 @@ class AOC2020 : BaseTest("AOC2020") {
         //part2
         foodWithAllergens.joinToString(",").log()
     }
+
+    @Test
+    fun day22() = test(2) { lines ->
+        val deck1 = lines.takeWhile { it.isNotBlank() }.drop(1).map { it.toInt() }.toMutableList()
+        val deck2 = lines.takeLastWhile { it.isNotBlank() }.drop(1).map { it.toInt() }.toMutableList()
+        day22part1(deck1.toMutableList(), deck2.toMutableList()).log()
+        day22part2(deck1.toMutableList(), deck2.toMutableList(), 1).log()
+    }
+
+    private fun day22part1(deck1: MutableList<Int>, deck2: MutableList<Int>): Pair<Boolean, Int> {
+        while (deck1.isNotEmpty() && deck2.isNotEmpty()) {
+            val card1 = deck1.removeAt(0)
+            val card2 = deck2.removeAt(0)
+            if (card1 > card2) {
+                deck1 += card1
+                deck1 += card2
+            } else {
+                deck2 += card2
+                deck2 += card1
+            }
+        }
+        return score(deck1, deck2)
+    }
+
+    private fun day22part2(deck1: MutableList<Int>, deck2: MutableList<Int>, level: Int): Pair<Boolean, Int> {
+        //log("$level $deck1 $deck2")
+        val deck1s = mutableListOf<List<Int>>()
+        val deck2s = mutableListOf<List<Int>>()
+        while (deck1.isNotEmpty() && deck2.isNotEmpty()) {
+            if (deck1 in deck1s && deck2 in deck2s) return Pair(true, 0)
+            deck1s.add(deck1.toList())
+            deck2s.add(deck2.toList())
+            val card1 = deck1.removeAt(0)
+            val card2 = deck2.removeAt(0)
+            //log("$level $card1 $card2")
+            val player1Wins = if (card1 <= deck1.size && card2 <= deck2.size) day22part2(deck1.take(card1).toMutableList(), deck2.take(card2).toMutableList(), level + 1).first else card1 > card2
+            if (player1Wins) {
+                deck1 += card1
+                deck1 += card2
+            } else {
+                deck2 += card2
+                deck2 += card1
+            }
+            //log("$level $deck1 $deck2")
+        }
+        return score(deck1, deck2)
+    }
+
+    private fun score(deck1: List<Int>, deck2: List<Int>): Pair<Boolean, Int> {
+        val player1Wins = deck1.isNotEmpty()
+        return player1Wins to (if (player1Wins) deck1 else deck2).reversed().mapIndexed { index, card -> (index + 1) * card }.sum()
+    }
 }
