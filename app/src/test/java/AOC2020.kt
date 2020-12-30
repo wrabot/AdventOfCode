5de09fe653f5455116ac1fc687eb8b6911042d78
Wilfrid Rabot <wilfird.rabot@gmail.com>
2020-12-30 02:10:31 +0100

    Improve 2020 day24 with solution day17

--- app/src/test/java/AOC2020.kt
+++ app/src/test/java/AOC2020.kt
@@ -794,34 +794,35 @@ class AOC2020 : BaseTest("AOC2020") {
     @Test
     fun day24() = test(3) { lines ->
         //part1
-        val blacks = mutableSetOf<Pair<Int, Int>>()
-        lines.forEach { it.goToTile(Pair(0, 0)).run { if (!blacks.remove(this)) blacks.add(this) } }
+        val blacks = mutableSetOf<Tile>()
+        lines.forEach { Tile(0, 0).goToTile(it).run { if (!blacks.remove(this)) blacks.add(this) } }
         blacks.size.log()
 
         //part2
-        repeat(100) {
-            val blacksNeighbors = blacks.map { it to neighbors(it).partition(blacks::contains) }
-            blacks.addAll(blacksNeighbors.flatMap { it.second.second }.filter { neighbors(it).count(blacks::contains) == 2 })
-            blacks.removeAll(blacksNeighbors.filter { it.second.first.size !in 1..2 }.map { it.first })
-        }
-        blacks.size.log()
+        val directions = listOf("e", "w", "ne", "nw", "se", "sw")
+        val neighbors = mutableMapOf<Tile, List<Tile>>()
+        repeat(100, blacks.toSet()) { tiles ->
+            tiles.flatMap { tile -> neighbors.getOrPut(tile) { directions.map { tile.goToTile(it) } } }
+                .groupingBy { it }.eachCount().filter { it.value == 2 || (it.value == 1 && it.key in tiles) }.keys
+        }.size.log()
     }
 
-    private fun neighbors(start: Pair<Int, Int>) = listOf("e", "w", "ne", "nw", "se", "sw").map { it.goToTile(start) }
-    private fun String.goToTile(start: Pair<Int, Int>): Pair<Int, Int> {
-        var x = start.first
-        var y = start.second
-        var previous: Char? = null
-        forEach {
-            when (it) {
-                'n' -> y--
-                's' -> y++
-                'e' -> if (previous != 's') x++
-                'w' -> if (previous != 'n') x--
+    data class Tile(val x: Int, val y: Int) {
+        fun goToTile(path: String): Tile {
+            var x = x
+            var y = y
+            var previous: Char? = null
+            path.forEach {
+                when (it) {
+                    'n' -> y--
+                    's' -> y++
+                    'e' -> if (previous != 's') x++
+                    'w' -> if (previous != 'n') x--
+                }
+                previous = it
             }
-            previous = it
+            return Tile(x, y)
         }
-        return Pair(x, y)
     }
 
     @Test
