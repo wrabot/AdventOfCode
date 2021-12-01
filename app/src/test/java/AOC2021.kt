4021e4bbc53ddde9f96789779e9260c39a91025f
Wilfrid Rabot <wilfrid.rabot@orange.com>
2021-12-01 07:20:34 +0100

    2021 day1

--- app/src/test/java/AOC2021.kt
+++ app/src/test/java/AOC2021.kt
@@ -2,6 +2,9 @@ import org.junit.Test
 
 class AOC2021 : BaseTest("AOC2021") {
     @Test
-    fun day1() = test(1) { lines ->
+    fun day1() = test(1, 2) { lines ->
+        val depths = lines.map { it.toInt() }
+        depths.zipWithNext().count { it.first < it.second }.log()
+        depths.windowed(3).map { it.sum() }.zipWithNext().count { it.first < it.second }.log()
     }
 }
