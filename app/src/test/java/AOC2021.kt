440c46eeda1271649879580e6fd8f130d5adec6f
Wilfrid Rabot <wilfrid.rabot@orange.com>
2021-12-04 06:54:53 +0100

    2021 day3

--- app/src/test/java/AOC2021.kt
+++ app/src/test/java/AOC2021.kt
@@ -46,4 +46,33 @@ class AOC2021 : BaseTest("AOC2021") {
         }
         (position * depth).log()
     }
+
+    @Test
+    fun day3() = test(1, 2) { lines ->
+        //part1
+        val length = lines[0].length
+        val threshold = lines.size / 2
+        val gammaBits = (0 until length).map { position ->
+            if (lines.count { it[position] == '1' } > threshold) '1' else '0'
+        }.joinToString("")
+        val epsilonBits = gammaBits.map { if (it == '1') '0' else '1' }.joinToString("")
+        (gammaBits.toInt(2) * epsilonBits.toInt(2)).log()
+
+        //part2
+        val oxygen = lines.day3Part2('0', '1')
+        val co2 = lines.day3Part2('1', '0')
+        (oxygen * co2).log()
+    }
+
+    private fun List<String>.day3Part2(mostZeroBit: Char, leastOrEqualZeroBit: Char) : Int {
+        val length = this[0].length
+        var current = this
+        for (position in 0 until length) {
+            if (current.size == 1) break
+            val threshold = current.size / 2
+            val bit = if (current.count { it[position] == '0' } > threshold) mostZeroBit else leastOrEqualZeroBit
+            current = current.filter { it[position] == bit }
+        }
+        return current.first().toInt(2)
+    }
 }
