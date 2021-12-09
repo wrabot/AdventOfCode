4f8cedefd0c2527bdc64300f175f2c16224b99dd
Wilfrid Rabot <wilfrid.rabot@orange.com>
2021-12-09 07:38:21 +0100

    Add board in tools

--- app/src/test/java/Tools.kt
+++ app/src/test/java/Tools.kt
@@ -1,8 +1,43 @@
+@file:Suppress("MemberVisibilityCanBePrivate", "unused")
+
 import java.math.BigInteger
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTimedValue
 
 data class Point(val x: Int, val y: Int)
+
+class Board<T>(val width: Int, val height: Int, val cells: List<T>) {
+    val points = (0 until height).flatMap { y ->
+        (0 until width).map { x ->
+            Point(x, y)
+        }
+    }
+
+    init {
+        if (cells.size != width * height) throw Error("invalid board")
+    }
+
+    fun isValid(point: Point) = point.y in 0 until height && point.x in 0 until width
+
+    operator fun get(point: Point) =
+        if (isValid(point)) cells[point.y * width + point.x] else throw Error("invalid point")
+
+    fun neighbors4(point: Point) =
+        listOfNotNull(
+            Point(point.x + 1, point.y),
+            Point(point.x - 1, point.y),
+            Point(point.x, point.y + 1),
+            Point(point.x, point.y - 1)
+        ).filter { isValid(it) }
+
+    fun neighbors8(point: Point) =
+        neighbors4(point) + listOfNotNull(
+            Point(point.x + 1, point.y + 1),
+            Point(point.x - 1, point.y + 1),
+            Point(point.x + 1, point.y - 1),
+            Point(point.x - 1, point.y - 1)
+        ).filter { isValid(it) }
+}
 
 fun log(message: Any?) = System.err.println("!!!$message")
 
