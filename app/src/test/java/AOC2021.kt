971c2191fb2d78090f6256fdf76d84c0d06115b0
Wilfrid Rabot <wilfrid.rabot@orange.com>
2021-12-09 16:07:30 +0100

    2021 day9

--- app/src/test/java/AOC2021.kt
+++ app/src/test/java/AOC2021.kt
@@ -251,4 +251,23 @@ class AOC2021 : BaseTest("AOC2021") {
         }.log()
 
     }
+
+    @Test
+    fun day9() = test(1, 2) { lines ->
+        val board = Board(lines[0].length, lines.size, lines.flatMap { line -> line.map { it.toString().toInt() } })
+
+        val lowPoints = board.points.filter { point ->
+            val height = board[point]
+            board.neighbors4(point).all { board[it] > height }
+        }
+
+        // part1
+        lowPoints.sumBy { board[it] + 1 }.log()
+
+        // part2
+        fun findBasin(basin: Set<Point>, point: Point): Set<Point> =
+            if (board[point] == 9 || point in basin) basin else board.neighbors4(point).fold(basin + point, ::findBasin)
+
+        lowPoints.map { findBasin(emptySet(), it).count() }.sortedDescending().take(3).reduce(Int::times).log()
+    }
 }
