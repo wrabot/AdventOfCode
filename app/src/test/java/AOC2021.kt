31de66898e329cea17f6db89b34505e36f8c8d1f
Wilfrid Rabot <wilfrid.rabot@orange.com>
2021-12-10 07:33:51 +0100

    2021 day10

--- app/src/test/java/AOC2021.kt
+++ app/src/test/java/AOC2021.kt
@@ -270,4 +270,47 @@ class AOC2021 : BaseTest("AOC2021") {
 
         lowPoints.map { findBasin(emptySet(), it).count() }.sortedDescending().take(3).reduce(Int::times).log()
     }
+
+    @Test
+    fun day10() = test(1, 2) { lines ->
+        val simplifiedChunks = lines.map { line ->
+            var chunk = line
+            while (true) {
+                val length = chunk.length
+                chunk = chunk.replace("()", "")
+                    .replace("[]", "")
+                    .replace("{}", "")
+                    .replace("<>", "")
+                if (length == chunk.length) break
+            }
+            chunk.toList()
+        }
+
+        //part1
+        simplifiedChunks.sumBy { chunk ->
+            when (chunk.find { it in listOf(')', ']', '}', '>') }) {
+                ')' -> 3
+                ']' -> 57
+                '}' -> 1197
+                '>' -> 25137
+                else -> 0  // not corrupted
+            }
+        }.log()
+
+        //part2
+        val scores = simplifiedChunks.mapNotNull { chunk ->
+            runCatching {
+                chunk.reversed().fold(0) { acc, c ->
+                    acc * 5 + when (c) {
+                        '(' -> 1
+                        '[' -> 2
+                        '{' -> 3
+                        '<' -> 4
+                        else -> throw Error("Corrupted")
+                    }
+                }
+            }.getOrNull()
+        }.sorted()
+        scores[scores.size / 2].log()
+    }
 }
