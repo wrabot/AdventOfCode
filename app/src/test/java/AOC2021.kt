4f17944c1c12d05b2d2c07734fb6279e29c37096
Wilfrid Rabot <wilfrid.rabot@orange.com>
2021-12-11 12:44:30 +0100

    2021 day11

--- app/src/test/java/AOC2021.kt
+++ app/src/test/java/AOC2021.kt
@@ -312,4 +312,49 @@ class AOC2021 : BaseTest("AOC2021") {
         }.sorted()
         scores[scores.size / 2].log()
     }
+
+    @Test
+    fun day11() = test(1, 2) { lines ->
+        data class Cell(var level: Int) {
+            var flash = false
+            override fun toString() = level.toString()
+        }
+
+        val board = Board(lines[0].length, lines.size, lines.flatMap { line -> line.map { Cell(it.toString().toInt()) } })
+
+        var flashes = 0
+        fun step() {
+            board.cells.forEach { it.level++ }
+            do {
+                var flash = false
+                board.points.forEach { point ->
+                    val cell = board[point]
+                    if (!cell.flash && cell.level > 9) {
+                        cell.flash = true
+                        flash = true
+                        flashes++
+                        board.neighbors8(point).forEach { board[it].level++ }
+                    }
+                }
+            } while (flash)
+            board.cells.forEach {
+                if (it.flash) {
+                    it.flash = false
+                    it.level = 0
+                }
+            }
+        }
+
+        // part1
+        repeat(100) { step() }
+        flashes.log()
+
+        // part2
+        var step = 100
+        while (board.cells.any { it.level != 0 }) {
+            step()
+            step++
+        }
+        step.log()
+    }
 }
