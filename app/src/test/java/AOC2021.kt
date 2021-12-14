4a60a83e404b9ed7971018eacf97e10a928ae76f
Wilfrid Rabot <wilfrid.rabot@orange.com>
2021-12-14 06:32:24 +0100

    2021 day13

--- app/src/test/java/AOC2021.kt
+++ app/src/test/java/AOC2021.kt
@@ -342,6 +342,33 @@ class AOC2021 : BaseTest("AOC2021") {
 
     @Test
     fun day13() = test(1, 2) { lines ->
+        val dots = lines.takeWhile { it != "" }.map { it.split(",").map { it.toInt() } }
+        val folds = lines.takeLastWhile { it != "" }.map {
+            it.split("=").let { (a, b) -> a.removePrefix("fold along ") to b.toInt() }
+        }
+
+        fun List<Pair<String, Int>>.foldDots() = fold(dots) { acc, f ->
+            val index = if (f.first == "x") 0 else 1
+            val value = f.second
+            acc.mapNotNull { dot ->
+                when (dot[index].compareTo(value)) {
+                    -1 -> dot[index]
+                    1 -> 2 * value - dot[index]
+                    else -> null
+                }?.let { if (index == 0) listOf(it, dot[1]) else listOf(dot[0], it) }
+            }.distinct()
+        }
+
+        // part 1
+        folds.take(1).foldDots().count().log()
+
+        // part 2
+        val code = folds.foldDots()
+        val width = code.maxOf { it[0] } + 1
+        val height = code.maxOf { it[1] } + 1
+        List(width * height) {
+            if (listOf(it % width, it / width) in code) '#' else ' '
+        }.log(width)
     }
 
     @Test
