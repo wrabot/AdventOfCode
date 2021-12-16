02edeb5445adb55c4f4584c7e1f6c74818f09bb4
Wilfrid Rabot <wilfrid.rabot@orange.com>
2021-12-16 05:59:33 +0100

    2021 day15

--- app/src/test/java/AOC2021.kt
+++ app/src/test/java/AOC2021.kt
@@ -397,6 +397,46 @@ class AOC2021 : BaseTest("AOC2021") {
 
     @Test
     fun day15() = test(1, 2) { lines ->
+        data class Cell(val risk: Int, var minRisk: Int = Int.MAX_VALUE) {
+            override fun toString() = risk.toString()
+        }
+
+        fun minRisk(cave: Board<Cell>): Int {
+            val start = cave.points.first()
+            val end = cave.points.last()
+            cave[start].minRisk = 0
+
+            val todo = mutableListOf(start)
+            while (todo.isNotEmpty()) {
+                val point = todo.removeAt(0)
+                val minRisk = cave[point].minRisk
+                cave.neighbors4(point).forEach {
+                    val neighbor = cave[it]
+                    val newRisk = neighbor.risk + minRisk
+                    if (newRisk < neighbor.minRisk) {
+                        neighbor.minRisk = newRisk
+                        if (it !in todo) todo.add(it)
+                    }
+                }
+                todo.sortBy { cave[it].minRisk + end.x - it.x + end.y - it.y }
+            }
+            return cave[end].minRisk
+        }
+
+        minRisk(Board(lines[0].length, lines.size, lines.flatMap { line -> line.map { Cell(it.toString().toInt()) } })).log()
+
+        minRisk(
+            Board(
+                lines[0].length * 5,
+                lines.size * 5,
+                (0..4).flatMap { row ->
+                    lines.flatMap { line ->
+                        (0..4).flatMap { column ->
+                            line.map { Cell((it.toString().toInt() + column + row - 1) % 9 + 1) }
+                        }
+                    }
+                })
+        ).log()
     }
 
     @Test
