a607f7f5344beac195472eb6b41f47e65086136a
Wilfrid Rabot <wilfrid.rabot@orange.com>
2021-12-17 18:39:20 +0100

    2021 day17

--- app/src/test/java/AOC2021.kt
+++ app/src/test/java/AOC2021.kt
@@ -518,7 +518,50 @@ class AOC2021 : BaseTest("AOC2021") {
     }
 
     @Test
-    fun day17() = test(1, 2) { lines ->
+    fun day17() {
+        fun maxY(vxInit: Int, vyInit: Int, xTarget: IntRange, yTarget: IntRange): Int? {
+            var vx = vxInit
+            var vy = vyInit
+            var x = 0
+            var y = 0
+            var maxY = 0
+            while (x <= xTarget.last && y >= yTarget.first) {
+                x += vx
+                y += vy
+                if (y > maxY) maxY = y
+                if (vx > 0) vx-- else if (vx < 0) vx++
+                vy--
+                if (x in xTarget && y in yTarget) return maxY
+            }
+            return null
+        }
+
+        fun findBestMaxY(xTarget: IntRange, yTarget: IntRange): Int {
+            var bestMaxY = 0
+            for (vy in 0..xTarget.last) {
+                for (vx in 0..xTarget.last) {
+                    bestMaxY = maxY(vx, vy, xTarget, yTarget)?.coerceAtLeast(bestMaxY) ?: bestMaxY
+                }
+            }
+            return bestMaxY
+        }
+
+        fun countVelocities(xTarget: IntRange, yTarget: IntRange): Int {
+            var velocities = 0
+            for (vy in yTarget.first..xTarget.last) {
+                for (vx in 0..xTarget.last) {
+                    if (maxY(vx, vy, xTarget, yTarget) != null) {
+                        velocities++
+                    }
+                }
+            }
+            return velocities
+        }
+
+        //findBestMaxY(20..30, -10..-5).log()
+        log("part 1: " + findBestMaxY(102..157, -146..-90))
+        //countVelocities(20..30, -10..-5).log()
+        log("part 2: " + countVelocities(102..157, -146..-90))
     }
 
     @Test
