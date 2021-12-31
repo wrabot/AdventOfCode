33c3170a279d0fce0aaee3b21984d6b3a5cf6cc9
Wilfrid Rabot <wilfrid.rabot@orange.com>
2021-12-31 09:36:37 +0100

    Improve 2019 days

--- src/main/kotlin/aoc2019/Day3.kt
+++ src/main/kotlin/aoc2019/Day3.kt
@@ -1,26 +1,29 @@
 package aoc2019
 
 import tools.Day
+import tools.Point
 import kotlin.math.absoluteValue
 
 class Day3(test: Int? = null) : Day(2019, 3, test) {
-    override fun solvePart1() = intersections.minOf { it.first.absoluteValue + it.second.absoluteValue }
+    override fun solvePart1() = intersections.minOf { it.x.absoluteValue + it.y.absoluteValue }
 
     override fun solvePart2() = intersections.minOf { point -> wires.sumOf { it.indexOf(point) } }
 
-    private val wires = lines.map { wire ->
-        wire.split(",").map { it.first() to it.drop(1).toInt() }.fold(listOf(0 to 0)) { acc, v ->
-            acc + acc.last().let { pos ->
-                when (v.first) {
-                    'R' -> (1..v.second).map { (pos.first + it) to pos.second }
-                    'L' -> (1..v.second).map { (pos.first - it) to pos.second }
-                    'U' -> (1..v.second).map { pos.first to (pos.second - it) }
-                    'D' -> (1..v.second).map { pos.first to (pos.second + it) }
+    private val start = Point(0, 0)
+
+    private val wires = lines.map { line ->
+        line.split(",").map { it.first() to it.drop(1).toInt() }.fold(listOf(start)) { acc, move ->
+            acc + acc.last().let { point ->
+                when (move.first) {
+                    'R' -> (1..move.second).map { Point(point.x + it, point.y) }
+                    'L' -> (1..move.second).map { Point(point.x - it, point.y) }
+                    'U' -> (1..move.second).map { Point(point.x, point.y - it) }
+                    'D' -> (1..move.second).map { Point(point.x, point.y + it) }
                     else -> error("invalid direction")
                 }
             }
         }
     }
 
-    private val intersections = wires.reduce { acc, list -> (acc intersect list).toList() }.minus(0 to 0).minus(0 to 0)
+    private val intersections = (wires[0].minus(start) intersect wires[1].toSet()).toList()
 }
