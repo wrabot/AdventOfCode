c5388dd6a087720ccdac45e0c431652b8a8d36d7
Wilfrid Rabot <wilfird.rabot@gmail.com>
2022-11-30 23:48:59 +0100

    2020 day 15

---
+++ src/main/kotlin/aoc2015/Day20.kt
@@ -0,0 +1,26 @@
+package aoc2015
+
+import tools.Day
+
+class Day20(test: Int? = null) : Day(2015, 20, test) {
+    override fun solvePart1(): Int {
+        var house = 2
+        while (true) {
+            if (presents(house) >= goal) return house
+            house++
+        }
+    }
+
+    override fun solvePart2() = Unit
+
+    private fun presents(house: Int): Int {
+        var presents = house
+        for (i in 1..(house + 1) / 2) {
+            if (house % i == 0) presents += i
+        }
+        return presents * 10
+    }
+
+
+    private val goal = input.toInt()
+}
