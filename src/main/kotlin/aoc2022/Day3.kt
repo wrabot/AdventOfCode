66609333ca4faa055dae2915e08e84ffa1fb9ada
Wilfrid Rabot <wilfird.rabot@gmail.com>
2022-12-03 07:16:05 +0100

    2022 day 3

---
+++ src/main/kotlin/aoc2022/Day3.kt
@@ -0,0 +1,10 @@
+package aoc2022
+
+import tools.Day
+
+class Day3(test: Int? = null) : Day(2022, 3, test) {
+    override fun solvePart1() = rucksacks.sumOf { it.chunked(it.size / 2).commonItem() }
+    override fun solvePart2() = rucksacks.chunked(3).sumOf { it.commonItem() }
+    private fun List<List<Int>>.commonItem() = map { it.toSet() }.reduce { acc, items -> acc.intersect(items) }.single()
+    private val rucksacks = lines.map { line -> line.map { if (it.isLowerCase()) (it - 'a' + 1) else it - 'A' + 27 } }
+}
--- src/main/kotlin/aoc2022/Year.kt
+++ src/main/kotlin/aoc2022/Year.kt
@@ -5,4 +5,6 @@ fun checkAll() {
     Day1().check(66487, 197301)
     Day2(1).check(15, 12)
     Day2().check(14375, 10274)
+    Day3(1).check(157, 70)
+    Day3().check(7811, 2639)
 }
