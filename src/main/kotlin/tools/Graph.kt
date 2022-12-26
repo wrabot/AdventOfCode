21cd564c7452f2b364992acbc63ab19280e348d9
Wilfrid Rabot <wilfrid.rabot@orange.com>
2022-12-26 11:01:48 +0100

    Improve shortPath

--- src/main/kotlin/tools/Graph.kt
+++ src/main/kotlin/tools/Graph.kt
@@ -1,29 +1,35 @@
 package tools
 
+import java.util.*
+
 fun <Node : Any> shortPath(start: Node, end: Node, neighbors: (Node) -> List<Node>) =
-    shortPath2(start, end, neighbors) { _, currentCost, _ -> currentCost + 1 }
+    shortPath(start, isEnd = { this == end }, neighbors = neighbors)
 
 fun <Node : Any> shortPath(start: Node, end: Node, cost: (Node) -> Int, neighbors: (Node) -> List<Node>) =
-    shortPath2(start, end, neighbors) { _, currentCost, destination -> currentCost + cost(destination) }
+    shortPath(start, isEnd = { this == end }, { _, currentCost, destination -> currentCost + cost(destination) }, neighbors)
 
-fun <Node : Any> shortPath2(start: Node, end: Node, neighbors: (Node) -> List<Node>, cost: (origin: Node, currentCost: Int, destination: Node) -> Int): List<Node> {
-    val todo = mutableMapOf(start to 0)
-    val done = mutableSetOf<Node>()
+fun <Node : Any> shortPath(
+    start: Node,
+    isEnd: Node.() -> Boolean,
+    cost: (origin: Node, currentCost: Int, destination: Node) -> Int = { _, currentCost, _ -> currentCost + 1 },
+    neighbors: Node.() -> List<Node>
+): List<Node> {
     val predecessor = mutableMapOf<Node, Node>()
+    val costs = mutableMapOf(start to 0)
+    val todo = PriorityQueue<Node>(compareBy { costs[it] })
+    todo.offer(start)
     while (true) {
-        val (currentNode, currentCost) = todo.minByOrNull { it.value } ?: break
-        if (currentNode == end) break
-        todo.remove(currentNode)
-        done.add(currentNode)
+        val currentNode = todo.poll() ?: break
+        if (currentNode.isEnd()) return generateSequence(currentNode) { predecessor[it] }.toList().reversed()
+        val currentCost = costs[currentNode]!!
         neighbors(currentNode).forEach { next ->
-            if (next !in done) {
-                val nextCost = cost(currentNode, currentCost, next)
-                if (nextCost < todo.getOrDefault(next, Int.MAX_VALUE)) {
-                    todo[next] = nextCost
-                    predecessor[next] = currentNode
-                }
+            val nextCost = cost(currentNode, currentCost, next)
+            if (nextCost < costs.getOrDefault(next, Int.MAX_VALUE)) {
+                costs[next] = nextCost
+                todo.offer(next)
+                predecessor[next] = currentNode
             }
         }
     }
-    return generateSequence(end) { predecessor[it] }.toList().reversed()
+    return emptyList()
 }
