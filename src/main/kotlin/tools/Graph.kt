b6c91f949571f44b2fecf02260dd91c44f03c9d5
Wilfrid Rabot <wilfrid.rabot@orange.com>
2023-01-05 13:09:38 +0100

    Refactor without tree map

--- src/main/kotlin/tools/Graph.kt
+++ src/main/kotlin/tools/Graph.kt
@@ -15,54 +15,51 @@ fun <Node : Any> shortPath(
     toEndMinimalCost: (Node) -> Int = { 0 }, // A*
     neighbors: Node.() -> List<Node>
 ): List<Node> {
-    val extendedStart = ExtendedNode(start, 0, toEndMinimalCost(start), null)
+    val extendedStart = ExtendedNode(start, 0, toEndMinimalCost(start), true)
     val extendedNodes = mutableMapOf(start to extendedStart)
-    val todo = mutableListOf<MutableList<ExtendedNode<Node>>>()
-    todo.add(extendedStart)
+    val todo = mutableListOf(extendedStart)
     while (true) {
-        val currentExtendedNode = todo.firstOrNull()?.first() ?: return emptyList()
+        val currentExtendedNode = todo.removeFirstOrNull() ?: return emptyList()
+        currentExtendedNode.todo = false
         if (currentExtendedNode.node.isEnd()) return generateSequence(currentExtendedNode) { it.predecessor }.map { it.node }.toList().reversed()
-        todo.remove(currentExtendedNode)
         neighbors(currentExtendedNode.node).forEach { nextNode ->
             val newFromStartCost = currentExtendedNode.fromStartCost + cost(currentExtendedNode.node, nextNode)
             val nextExtendedNode = extendedNodes[nextNode]
-            if (nextExtendedNode == null) {
-                val extendedNode = ExtendedNode(nextNode, newFromStartCost, newFromStartCost + toEndMinimalCost(nextNode), currentExtendedNode)
-                extendedNodes[nextNode] = extendedNode
-                todo.add(extendedNode)
-            } else if (newFromStartCost < nextExtendedNode.fromStartCost) {
-                nextExtendedNode.predecessor = currentExtendedNode
-                nextExtendedNode.fromStartCost = newFromStartCost
-                val newToEndMinimalCost = newFromStartCost + toEndMinimalCost(nextNode)
-                if (nextExtendedNode.minimalCost != newToEndMinimalCost) {
-                    todo.remove(nextExtendedNode)
-                    nextExtendedNode.minimalCost = newToEndMinimalCost
-                    todo.add(nextExtendedNode)
+            when {
+                nextExtendedNode == null -> {
+                    val extendedNode = ExtendedNode(nextNode, newFromStartCost, newFromStartCost + toEndMinimalCost(nextNode), true)
+                    extendedNode.predecessor = currentExtendedNode
+                    extendedNodes[nextNode] = extendedNode
+                    todo.add(-todo.binarySearch(extendedNode) - 1, extendedNode)
+                }
+                newFromStartCost < nextExtendedNode.fromStartCost -> {
+                    var toIndex = todo.size
+                    if (nextExtendedNode.todo) {
+                        toIndex = todo.binarySearch(nextExtendedNode)
+                        todo.removeAt(toIndex)
+                    }
+                    nextExtendedNode.predecessor = currentExtendedNode
+                    nextExtendedNode.minimalCost -= nextExtendedNode.fromStartCost - newFromStartCost
+                    nextExtendedNode.fromStartCost = newFromStartCost
+                    nextExtendedNode.todo = true
+                    todo.add(-todo.binarySearch(nextExtendedNode, toIndex = toIndex) - 1, nextExtendedNode)
                 }
             }
         }
     }
 }
 
-private data class ExtendedNode<Node : Any>(val node: Node, var fromStartCost: Int, var minimalCost: Int, var predecessor: ExtendedNode<Node>?)
+private data class ExtendedNode<Node : Any>(val node: Node, var fromStartCost: Int, var minimalCost: Int, var todo: Boolean) : Comparable<ExtendedNode<Node>> {
+    var predecessor: ExtendedNode<Node>? = null
 
-private fun <Node : Any> MutableList<MutableList<ExtendedNode<Node>>>.add(node: ExtendedNode<Node>) {
-    val index = binarySearch { it.first().minimalCost - node.minimalCost }
-    if (index >= 0) {
-        this[index].add(node)
-    } else {
-        add(-index - 1, mutableListOf(node))
+    override fun compareTo(other: ExtendedNode<Node>): Int {
+        val compare = minimalCost.compareTo(other.minimalCost)
+        return if (compare != 0) compare else id.compareTo(other.id)
     }
-}
 
-private fun <Node : Any> MutableList<MutableList<ExtendedNode<Node>>>.remove(node: ExtendedNode<Node>) {
-    val index = binarySearch { it.first().minimalCost - node.minimalCost }
-    if (index >= 0) {
-        val list = this[index]
-        if (list.size <= 1) {
-            removeAt(index)
-        } else {
-            list.remove(node)
-        }
+    private val id = nextId++
+
+    companion object {
+        private var nextId = Int.MIN_VALUE
     }
 }
