f03abb87d39e8897bf9364bb1c86a1f5af5a898c
Wilfrid Rabot <wilfrid.rabot@orange.com>
2023-11-30 18:08:01 +0100

    year 2019 day 7

---
+++ src/main/kotlin/aoc2019/Day7.kt
@@ -0,0 +1,66 @@
+package aoc2019
+
+import tools.Day
+import tools.combinations
+
+class Day7(test: Int? = null) : Day(2019, 7, test) {
+    override fun solvePart1() = (0..4).toList().combinations().maxOf { phases ->
+        val runtimes = List(5) { Runtime(code).apply { execute(phases[it]) } }
+        runtimes.fold(0) { input, runtime -> runtime.execute(input)!! }
+    }
+
+    override fun solvePart2() = (5..9).toList().combinations().maxOf { phases ->
+        val runtimes = List(5) { Runtime(code).apply { execute(phases[it]) } }
+        var current: Int? = 0
+        while (true) {
+            current = runtimes.fold(current) { input, runtime -> runtime.execute(input) } ?: break
+        }
+        current ?: 0
+    }
+
+    private val code = lines.first().split(",").map { it.toInt() }
+
+    class Runtime(code: List<Int>) {
+        private val memory = code.toMutableList()
+        private var offset: Int = 0
+
+        fun execute(input: Int?): Int? {
+            var i = input
+            while(true) {
+                if (offset >= memory.size) error("no opcode")
+                val opcode = read()
+                val firstMode = opcode / 100 % 10
+                val secondMode = opcode / 1000 % 10
+                when (opcode % 100) {
+                    1 -> read3().run { memory[third] = first.toValue(firstMode) + second.toValue(secondMode) }
+                    2 -> read3().run { memory[third] = first.toValue(firstMode) * second.toValue(secondMode) }
+                    3 -> with (i ?: break) {
+                        memory[read()] = this
+                        i = null
+                    }
+
+                    4 -> return read().toValue(firstMode)
+                    5 -> read2().run { if (first.toValue(firstMode) != 0) offset = second.toValue(secondMode) }
+                    6 -> read2().run { if (first.toValue(firstMode) == 0) offset = second.toValue(secondMode) }
+                    7 -> read3().run {
+                        memory[third] = if (first.toValue(firstMode) < second.toValue(secondMode)) 1 else 0
+                    }
+
+                    8 -> read3().run {
+                        memory[third] = if (first.toValue(firstMode) == second.toValue(secondMode)) 1 else 0
+                    }
+
+                    99 -> break
+                    else -> error("invalid opcode $opcode")
+                }
+            }
+            offset-- // stay on instruction
+            return null
+        }
+
+        private fun read() = memory[offset++]
+        private fun read2() = Pair(read(), read())
+        private fun read3() = Triple(read(), read(), read())
+        private fun Int.toValue(mode: Int) = if (mode == 1) this else memory[this]
+    }
+}
--- src/main/kotlin/aoc2019/Year.kt
+++ src/main/kotlin/aoc2019/Year.kt
@@ -1,6 +1,7 @@
 package aoc2019
 
 fun checkAll() {
+    /*
     Day1().check(3452245, 5175499)
     Day2().check(3516593, 7749)
     //Day3(1).check(6,30)
@@ -10,4 +11,11 @@ fun checkAll() {
     Day4().check(1790, 1206)
     Day5().check(15097178, 1558663)
     Day6().check(130681, 313)
+    */
+    Day7().check(116680, 89603079)
+    Day7(1).checkPart1(43210)
+    Day7(2).checkPart1(54321)
+    Day7(3).checkPart1(65210)
+    Day7(4).checkPart2(139629729)
+    Day7(5).checkPart2(18216)
 }
