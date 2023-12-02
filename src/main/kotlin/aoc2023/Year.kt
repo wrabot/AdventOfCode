b3fc63bf95660192880c8d1689709c8a0fb95ffd
Wilfrid Rabot <wilfrid.rabot@orange.com>
2023-12-02 19:36:03 +0100

    prepare next days

--- src/main/kotlin/aoc2023/Year.kt
+++ src/main/kotlin/aoc2023/Year.kt
@@ -1,7 +1,8 @@
 package aoc2023
 
 fun checkAll() {
-    Day1().check(53080,53268)
-    Day2().check(2727,56580)
-    Day2(1).check(8,2286)
+    Day1().check(53080, 53268)
+    Day2().check(2727, 56580)
+    Day2(1).check(8, 2286)
+    Day3().check(Unit, Unit)
 }
