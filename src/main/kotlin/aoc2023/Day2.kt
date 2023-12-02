11a839ee6606080b3559c91c9d65144856482e32
Wilfrid Rabot <wilfrid.rabot@orange.com>
2023-12-02 19:33:40 +0100

    year 2023 day 2

--- src/main/kotlin/aoc2023/Day2.kt
+++ src/main/kotlin/aoc2023/Day2.kt
@@ -2,7 +2,33 @@ package aoc2023
 
 import tools.Day
 
-class Day2 : Day(2023, 2) {
-    override fun solvePart1() = Unit
-    override fun solvePart2() = Unit
+class Day2(test: Int? = null) : Day(2023, 2, test) {
+    override fun solvePart1() = games.filter { game ->
+        game.subsets.all { it.red <= 12 } && game.subsets.all { it.green <= 13 } && game.subsets.all { it.blue <= 14 }
+    }.sumOf { it.id }
+
+    override fun solvePart2() = games.sumOf { game ->
+        game.subsets.maxOf { it.red } * game.subsets.maxOf { it.green } * game.subsets.maxOf { it.blue }
+    }
+
+    // data
+
+    private val games = lines.mapIndexed { id, line ->
+        Game(id + 1, line.split(":")[1].split(";").map { subset ->
+            var red = 0
+            var green = 0
+            var blue = 0
+            subset.split(",").forEach { group ->
+                red += group.getColor("red")
+                green += group.getColor("green")
+                blue += group.getColor("blue")
+            }
+            Subset(red, green, blue)
+        })
+    }
+
+    data class Game(val id: Int, val subsets: List<Subset>)
+    data class Subset(val red: Int, val green: Int, val blue: Int)
+
+    private fun String.getColor(color: String) = if (endsWith(color)) removeSuffix(color).trim().toInt() else 0
 }
--- src/main/kotlin/aoc2023/Year.kt
+++ src/main/kotlin/aoc2023/Year.kt
@@ -2,4 +2,6 @@ package aoc2023
 
 fun checkAll() {
     Day1().check(53080,53268)
+    Day2().check(2727,56580)
+    Day2(1).check(8,2286)
 }
