64404565b807ee59fe1f05c0acc02a1a260c471e
Wilfrid Rabot <wilfrid.rabot@orange.com>
2023-12-03 19:31:10 +0100

    year 2019 day 9

---
+++ src/main/kotlin/aoc2019/Day9.kt
@@ -0,0 +1,95 @@
+package aoc2019
+
+import tools.Day
+
+class Day9(test: Int? = null) : Day(2019, 9, test) {
+    override fun solvePart1() = Runtime(code).run { generateSequence { execute(1) } }.toList()
+
+    override fun solvePart2() = Runtime(code).execute(2)!!
+
+    private val code = lines.first().split(",").map { it.toLong() }
+
+    class Runtime(code: List<Long>) {
+        private var offset: Long = 0
+        private var relative: Long = 0
+
+        fun execute(input: Long?): Long? {
+            var i = input
+            while (true) {
+                val opcode = read().toInt()
+                val firstMode = opcode / 100 % 10
+                val secondMode = opcode / 1000 % 10
+                val thirdMode = opcode / 10000 % 10
+                when (opcode % 100) {
+                    1 -> read3().run {
+                        set(
+                            third.toAddress(thirdMode),
+                            first.toValue(firstMode) + second.toValue(secondMode)
+                        )
+                    }
+
+                    2 -> read3().run {
+                        set(
+                            third.toAddress(thirdMode),
+                            first.toValue(firstMode) * second.toValue(secondMode)
+                        )
+                    }
+
+                    3 -> with(i ?: break) {
+                        set(read().toAddress(firstMode), this)
+                        i = null
+                    }
+
+                    4 -> return read().toValue(firstMode)
+                    5 -> read2().run { if (first.toValue(firstMode) != 0L) offset = second.toValue(secondMode) }
+                    6 -> read2().run { if (first.toValue(firstMode) == 0L) offset = second.toValue(secondMode) }
+                    7 -> read3().run {
+                        set(
+                            third.toAddress(thirdMode),
+                            if (first.toValue(firstMode) < second.toValue(secondMode)) 1 else 0
+                        )
+                    }
+
+                    8 -> read3().run {
+                        set(
+                            third.toAddress(thirdMode),
+                            if (first.toValue(firstMode) == second.toValue(secondMode)) 1 else 0
+                        )
+                    }
+
+                    9 -> relative += read().toValue(firstMode)
+
+                    99 -> break
+                    else -> error("invalid opcode $opcode")
+                }
+            }
+            offset-- // stay on instruction
+            return null
+        }
+
+        private fun read() = get(offset++)
+        private fun read2() = Pair(read(), read())
+        private fun read3() = Triple(read(), read(), read())
+        private fun Long.toValue(mode: Int) = when (mode) {
+            0 -> get(this)
+            1 -> this
+            2 -> get(relative + this)
+            else -> error("invalid mode $mode")
+        }
+
+        private fun Long.toAddress(mode: Int) = when (mode) {
+            0 -> this
+            2 -> relative + this
+            else -> error("invalid mode $mode")
+        }
+
+        private var memory = mutableMapOf<Long, Long>().apply {
+            code.forEachIndexed { index, value -> set(index.toLong(), value) }
+        }
+
+        private fun get(index: Long) = memory.getOrDefault(index, 0)
+        private fun set(index: Long, value: Long) {
+            memory[index] = value
+        }
+    }
+}
--- src/main/kotlin/aoc2019/Year.kt
+++ src/main/kotlin/aoc2019/Year.kt
@@ -1,7 +1,6 @@
 package aoc2019
 
 fun checkAll() {
-    /*
     Day1().check(3452245, 5175499)
     Day2().check(3516593, 7749)
     //Day3(1).check(6,30)
@@ -17,11 +16,10 @@ fun checkAll() {
     Day7(3).checkPart1(65210)
     Day7(4).checkPart2(139629729)
     Day7(5).checkPart2(18216)
-     */
     Day8().check(
         1584,
         """
-        
+
         #  #  ##   ##  ####  ##  
         # #  #  # #  # #    #  # 
         ##   #    #    ###  #    
@@ -30,4 +28,8 @@ fun checkAll() {
         #  #  ##   ### ####  ##  
         """.trimIndent()
     )
+    Day9(1).checkPart1(listOf(109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99))
+    Day9(2).checkPart1(listOf(1219070632396864))
+    Day9(3).checkPart1(listOf(1125899906842624))
+    Day9().check(listOf(2775723069), 49115)
 }
