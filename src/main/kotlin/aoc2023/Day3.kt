9dcd763068b399156c585681484bb7ae8b6425f2
Wilfrid Rabot <wilfrid.rabot@orange.com>
2023-12-03 07:06:54 +0100

    year 2023 day 3

--- src/main/kotlin/aoc2023/Day3.kt
+++ src/main/kotlin/aoc2023/Day3.kt
@@ -3,6 +3,41 @@ package aoc2023
 import tools.Day
 
 class Day3(test: Int? = null) : Day(2023, 3, test) {
-    override fun solvePart1() = Unit
-    override fun solvePart2() = Unit
+    private val height = lines.count()
+    private val width = lines[0].count()
+    private val yIndices = 0 until height
+    private val xIndices = 0 until width
+    private val numberRegex = Regex("\\d+")
+
+    override fun solvePart1() = lines
+        .mapIndexed { y, line ->
+            numberRegex.findAll(line).sumOf { if (isNextToSymbol(it.range, y)) it.value.toInt() else 0 }
+        }
+        .sum()
+
+    private fun isNextToSymbol(x: IntRange, y: Int) =
+        (y - 1..y + 1).intersect(yIndices).any { r ->
+            (x.first - 1..x.last + 1).intersect(xIndices).any { c ->
+                lines[r][c].let { !it.isDigit() && it != '.' }
+            }
+        }
+
+    override fun solvePart2() = lines
+        .flatMapIndexed { y, line ->
+            numberRegex.findAll(line).flatMap { result ->
+                getGears(result.range, y).map { gear ->
+                    gear to result.value.toInt()
+                }
+            }
+        }
+        .groupBy({ it.first }, { it.second })
+        .map { if (it.value.count() == 2) it.value.first() * it.value.last() else 0 }
+        .sum()
+
+    private fun getGears(x: IntRange, y: Int) =
+        (y - 1..y + 1).intersect(yIndices).flatMap { r ->
+            (x.first - 1..x.last + 1).intersect(xIndices).mapNotNull { c ->
+                if (lines[r][c] == '*') r to c else null
+            }
+        }
 }
--- src/main/kotlin/aoc2023/Year.kt
+++ src/main/kotlin/aoc2023/Year.kt
@@ -2,7 +2,8 @@ package aoc2023
 
 fun checkAll() {
     Day1().check(53080, 53268)
-    Day2().check(2727, 56580)
     Day2(1).check(8, 2286)
-    Day3().check(Unit, Unit)
+    Day2().check(2727, 56580)
+    Day3(1).check(4361, 467835)
+    Day3().check(509115, 75220503)
 }
