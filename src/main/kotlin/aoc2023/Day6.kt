32050da31dcf33a4a5ed72f14b4df3b970a17a0a
Wilfrid Rabot <wilfrid.rabot@orange.com>
2023-12-06 06:54:38 +0100

    year 2023 day 6

--- src/main/kotlin/aoc2023/Day6.kt
+++ src/main/kotlin/aoc2023/Day6.kt
@@ -1,8 +1,30 @@
 package aoc2023
 
 import tools.Day
+import tools.toWords
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.sqrt
 
 class Day6(test: Int? = null) : Day(2023, 6, test) {
-    override fun solvePart1() = Unit
-    override fun solvePart2() = Unit
+    override fun solvePart1() = racesPart1.fold(1) { acc, race -> acc * race.records1() }
+
+    private val racesPart1 = lines[0].toWords().zip(lines[1].toWords()).drop(1)
+        .map { Race(it.first.toLong(), it.second.toLong()) }
+
+    override fun solvePart2() = racePart2.records2().toLong()
+
+    private val racePart2 = Race(
+        lines[0].removePrefix("Time:").replace(" ", "").toLong(),
+        lines[1].removePrefix("Distance:").replace(" ", "").toLong()
+    )
+
+    data class Race(val time: Long, val distance: Long) {
+        fun records1() = (1..<time).count { (time - it) * it > distance }
+        fun records2(): Double {
+            val b = time.toDouble() / 2
+            val d = sqrt(b * b - distance)
+            return ceil(b + d) - floor(b - d) - 1
+        }
+    }
 }
--- src/main/kotlin/aoc2023/Year.kt
+++ src/main/kotlin/aoc2023/Year.kt
@@ -1,7 +1,6 @@
 package aoc2023
 
 fun checkAll() {
-    /*
     Day1().check(53080, 53268)
     Day2(1).check(8, 2286)
     Day2().check(2727, 56580)
@@ -9,7 +8,8 @@ fun checkAll() {
     Day3().check(509115, 75220503)
     Day4(1).check(13, 30)
     Day4().check(25571, 8805731)
-     */
     Day5(1).check(35, 46)
     Day5().check(462648396, 2520479)
+    Day6(1).check(288, 71503)
+    Day6().check(131376, 34123437)
 }
