09560a9afd18a36d4d09c47520ebcf3a73382d99
Wilfrid Rabot <wilfrid.rabot@orange.com>
2023-12-11 07:42:24 +0100

    prepare next days

---
+++ src/main/kotlin/aoc2023/Day13.kt
@@ -0,0 +1,8 @@
+package aoc2023
+
+import tools.Day
+
+class Day13(test: Int? = null) : Day(2023, 13, test) {
+    override fun solvePart1() = Unit
+    override fun solvePart2() = Unit
+}
---
+++ src/main/kotlin/aoc2023/Day15.kt
@@ -0,0 +1,8 @@
+package aoc2023
+
+import tools.Day
+
+class Day15(test: Int? = null) : Day(2023, 14, test) {
+    override fun solvePart1() = Unit
+    override fun solvePart2() = Unit
+}
