06a94f878fe27b48784129b9f6e56a5cd62e7b3e
Wilfrid Rabot <wilfrid.rabot@orange.com>
2023-12-13 09:57:25 +0100

    Fix year 2023 day 8 test 2

--- src/main/kotlin/aoc2023/Day8.kt
+++ src/main/kotlin/aoc2023/Day8.kt
@@ -3,10 +3,10 @@ package aoc2023
 import tools.Day
 
 class Day8(test: Int? = null) : Day(2023, 8, test) {
-    override fun solvePart1() = path("AAA") { it == "ZZZ" }.count() * directions.size
+    override fun solvePart1() = (path("AAA") { it == "ZZZ" }.count() - 1) * directions.size
 
     override fun solvePart2() = map.keys.filter(::isStart).map { path(it, ::isEnd).checkPath() }
-        .fold(directions.size.toLong()) { acc, path -> acc * path.count() }
+        .fold(directions.size.toLong()) { acc, path -> acc * (path.count() - 1) }
 
     // Assumption : on a path, stays on same exit after the same number of steps
     private fun Sequence<String>.checkPath() = apply { assert(path(last()) { it == last() }.count() == count()) }
@@ -14,7 +14,7 @@ class Day8(test: Int? = null) : Day(2023, 8, test) {
     private fun isStart(s: String) = s.endsWith('A')
     private fun isEnd(s: String) = s.endsWith('Z')
     private fun path(start: String, isEnd: (String) -> Boolean) =
-        generateSequence(start) { shortcuts[it]!!.takeUnless(isEnd) }
+        generateSequence(start) { if (isEnd(it)) null else shortcuts[it] }
 
     private val directions = lines[0].map { if (it == 'L') 0 else 1 }
     private val map = lines.drop(2).associate {
