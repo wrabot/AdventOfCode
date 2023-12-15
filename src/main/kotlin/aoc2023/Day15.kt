65df5732e8a4c86a16899cb1af2876218dd2e02c
Wilfrid Rabot <wilfrid.rabot@orange.com>
2023-12-15 06:42:11 +0100

    year 2023 day 15

--- src/main/kotlin/aoc2023/Day15.kt
+++ src/main/kotlin/aoc2023/Day15.kt
@@ -3,6 +3,27 @@ package aoc2023
 import tools.Day
 
 class Day15(test: Int? = null) : Day(2023, 15, test) {
-    override fun solvePart1() = Unit
-    override fun solvePart2() = Unit
+    override fun solvePart1() = input.split(',').sumOf { it.hash() }
+    override fun solvePart2(): Int {
+        val boxes = Array(256) { mutableListOf<Pair<String, Int>>() }
+        input.split(',').forEach { step ->
+            val split = step.split('-', '=')
+            val label = split[0]
+            val box = boxes[label.hash()]
+            val boxIndex = box.indexOfFirst { it.first == label }
+            if (split[1].isNotEmpty()) {
+                val lens = label to split[1].toInt()
+                if (boxIndex >= 0) box[boxIndex] = lens else box.add(lens)
+            } else {
+                if (boxIndex >= 0) box.removeAt(boxIndex)
+            }
+        }
+        return boxes.mapIndexed { boxIndex, box ->
+            (boxIndex + 1) * box.mapIndexed { lensIndex, lens ->
+                (lensIndex + 1) * lens.second
+            }.sum()
+        }.sum()
+    }
+
+    private fun String.hash() = fold(0) { acc, c -> (acc + c.code) * 17 % 256 }
 }
