46c391b7f074b3ebae1040fe3abbc313ca3286ed
Wilfrid Rabot <wilfrid.rabot@orange.com>
2023-12-21 23:05:04 +0100

    year 2023 day 21

--- src/main/kotlin/aoc2023/Day21.kt
+++ src/main/kotlin/aoc2023/Day21.kt
@@ -1,8 +1,39 @@
 package aoc2023
 
-import tools.Day
+import tools.*
 
 class Day21(test: Int? = null) : Day(2023, 21, test) {
-    override fun solvePart1() = Unit
-    override fun solvePart2() = Unit
+    override fun solvePart1(): Int {
+        var tiles = setOf(garden.points.first { garden[it].c == 'S' })
+        repeat(64) { step ->
+            tiles.forEach { garden[it].step = false }
+            tiles = tiles.flatMap { point -> garden.neighbors4(point).filter { garden[it].c != '#' } }.toSet()
+            tiles.forEach { garden[it].step = true }
+        }
+        return tiles.count()
+    }
+
+    override fun solvePart2(): Long {
+        val target = 26501365
+        val size = garden.width // input is square and intrinsic period is size !
+        var tiles = setOf(garden.points.first { garden[it].c == 'S' })
+        repeat(target % size) { tiles = tiles.next() }
+        return List(3) {
+            if (it != 0) repeat(size) { tiles = tiles.next() }
+            tiles.count().toDouble()
+        }.polynomialCoefficients().polynomial((target / size - 2).toDouble()).toLong()
+    }
+    
+    fun Set<Point>.next() = flatMap { tile ->
+        garden.directions4.map { tile + it }.filter { garden[it.mod()].c != '#' }
+    }.toSet()
+
+    private fun Point.mod() = Point(x.mod(garden.width), y.mod(garden.height))
+
+    data class Cell(val c: Char) {
+        var step = false
+        override fun toString() = if (step) "O" else c.toString()
+    }
+
+    private val garden = Board(lines[0].length, lines.size, lines.flatMap { line -> line.map { Cell(it) } })
 }
