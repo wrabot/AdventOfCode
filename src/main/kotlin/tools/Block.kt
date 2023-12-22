8f00d405303bc5b54839851ca17f043d79395a1b
Wilfrid Rabot <wilfrid.rabot@orange.com>
2023-12-22 08:59:26 +0100

    Improve tools

--- src/main/kotlin/tools/Block.kt
+++ src/main/kotlin/tools/Block.kt
@@ -7,4 +7,15 @@ data class Block(val start: Point, val end: Point) {
     ).takeUnless { it.start.x > it.end.x || it.start.y > it.end.y || it.start.z > it.end.z }
 
     fun size() = (end.x.toLong() - start.x + 1) * (end.y.toLong() - start.y + 1) * (end.z.toLong() - start.z + 1)
+
+    init {
+        require((end - start).run { x >= 0 && y >= 0 && z >= 0 }) { "start not before end $this" }
+    }
+
+    fun hasIntersection(other: Block) = xRange.hasIntersection(other.xRange) &&
+            yRange.hasIntersection(other.yRange) && zRange.hasIntersection(other.zRange)
+
+    val xRange = start.x..end.x
+    val yRange = start.y..end.y
+    val zRange = start.z..end.z
 }
--- src/main/kotlin/tools/Range.kt
+++ src/main/kotlin/tools/Range.kt
@@ -1,7 +1,21 @@
+@file:Suppress("unused")
+
 package tools
 
-import java.lang.Integer.max
+import kotlin.math.max
 import kotlin.math.min
+
+fun IntRange.size() = last - first + 1
+fun LongRange.size() = last - first + 1
+
+fun IntRange.move(offset: Int) = first + offset..last + offset
+fun LongRange.move(offset: Long) = first + offset..last + offset
+
+fun IntRange.hasIntersection(other: IntRange) = first in other || last in other || other.first in this
+fun LongRange.hasIntersection(other: LongRange) = first in other || last in other || other.first in this
+
+fun rangeMinMax(a: Int, b: Int) = min(a, b)..max(a, b)
+fun rangeMinMax(a: Long, b: Long) = min(a, b)..max(a, b)
 
 fun List<IntRange>.merge() = mutableListOf<IntRange>().also { merge ->
     sortedBy { it.first }.forEach {
@@ -14,10 +28,6 @@ fun List<IntRange>.merge() = mutableListOf<IntRange>().also { merge ->
     }
 }
 
-fun rangeMinMax(a: Int, b: Int) = min(a, b)..max(a, b)
-
-fun IntRange.size() = last - first + 1
-fun LongRange.size() = last - first + 1
 
 fun <T : Comparable<T>> OpenEndRange<T>.splitWith(other: OpenEndRange<T>) = when {
     start >= other.endExclusive -> Triple(null, null, this)
