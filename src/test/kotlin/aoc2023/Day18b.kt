4bde5e3988597d3c6aefb974fccbea294184d501
Wilfrid Rabot <wilfrid.rabot@orange.com>
2024-01-22 13:48:53 +0100

    Migrate to tools 0.12 (polygon)

--- src/test/kotlin/aoc2023/Day18b.kt
+++ src/test/kotlin/aoc2023/Day18b.kt
@@ -4,23 +4,20 @@ import Day
 import tools.board.Direction4
 import tools.board.Direction4.*
 import tools.geometry.Point
-import kotlin.math.abs
+import tools.geometry.Polygon
 
 class Day18b(test: Int? = null) : Day(test) {
     override fun solvePart1() = movesPart1.solve()
 
     override fun solvePart2() = movesPart2.solve()
 
-    // "border area" + "shoelace area" = ((sum of border) + (sum of determinants)) / 2 + 1
-    // "border area" = (sum of border) / 2 + 1 (because there are only 90° turn)
+    // "border area" = perimeter / 2 + 1 (because there are only 90° turn)
     //     "straight border" = 1/2, "convex turn" = 3/4, "concave turn" =  1/4
     //     as there are only 90° turn, there 4 convex turns more than concave turns because sum of angles must 360°
     //     "convex turn" + "concave turn" = 2 "straight border"
-    //     so, "border area" => ((sum of border) - 4) / 2 + 4 * 3 / 4 => (sum of border) / 2 + 1
-    // "shoelace area" = (sum of determinants) / 2
-    private fun List<Point>.solve() = runningFold(Point.Zero) { acc, move -> acc + move }.zipWithNext().run {
-        sumOf { (it.first - it.second).manhattan() } + abs(sumOf { it.first.x * it.second.y - it.first.y * it.second.x })
-    }.toLong() / 2 + 1
+    //     so, "border area" => (perimeter - 4) / 2 + 4 * 3 / 4 => perimeter / 2 + 1
+    private fun List<Point>.solve() =
+        Polygon(runningFold(Point.Zero) { acc, move -> acc + move }).run { perimeter / 2 + 1 + area }.toLong()
 
     private val regex = "(.) (.*) \\(#(.*)\\)".toRegex()
     private val movesPart1 = lines.map { line ->
