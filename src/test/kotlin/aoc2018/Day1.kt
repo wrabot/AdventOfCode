9b8d8050d07322d0c928610970adaa68458a65f5
Wilfrid Rabot <wilfrid.rabot@orange.com>
2024-03-21 01:22:13 +0100

    year 2018 day 1

---
+++ src/test/kotlin/aoc2018/Day1.kt
@@ -0,0 +1,20 @@
+package aoc2018
+
+import Day
+
+class Day1(test: Int? = null) : Day(test) {
+    override fun solvePart1() = values.sum()
+    
+    val cache = mutableSetOf(0)
+    override fun solvePart2(): Int {
+        var current = 0
+        while (true) {
+            values.forEach {
+                current += it
+                if (!cache.add(current)) return current
+            }
+        }
+    }
+
+    val values = lines.map { it.toInt() }
+}
---
+++ src/test/kotlin/aoc2018/Day25.kt
@@ -0,0 +1,8 @@
+package aoc2018
+
+import Day
+
+class Day25(test: Int? = null) : Day(test) {
+    override fun solvePart1() = Unit
+    override fun solvePart2() = Unit
+}
