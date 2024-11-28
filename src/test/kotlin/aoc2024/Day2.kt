b84e7954b29ccf6d2d9dff63c93553dcfc6f643e
Wilfrid Rabot <wilfrid.rabot@orange.com>
2024-11-28 10:09:24 +0100

    Add year 2024

---
+++ src/test/kotlin/aoc2024/Day2.kt
@@ -0,0 +1,8 @@
+package aoc2024
+
+import Day
+
+class Day2(test: Int? = null) : Day(test) {
+    override fun solvePart1() = Unit
+    override fun solvePart2() = Unit
+}
---
+++ src/test/kotlin/aoc2024/Year.kt
@@ -0,0 +1,80 @@
+package aoc2024
+
+import kotlin.test.Test
+
+class Year2024 {
+    @Test
+    fun day1() = Day1().check(Unit, Unit)
+
+    @Test
+    fun day2() = Day2().check(Unit, Unit)
+
+    @Test
+    fun day3() = Day3().check(Unit, Unit)
+
+    @Test
+    fun day4() = Day4().check(Unit, Unit)
+
+    @Test
+    fun day5() = Day5().check(Unit, Unit)
+
+    @Test
+    fun day6() = Day6().check(Unit, Unit)
+
+    @Test
+    fun day7() = Day7().check(Unit, Unit)
+
+    @Test
+    fun day8() = Day8().check(Unit, Unit)
+
+    @Test
+    fun day9() = Day9().check(Unit, Unit)
+
+    @Test
+    fun day10() = Day10().check(Unit, Unit)
+
+    @Test
+    fun day11() = Day11().check(Unit, Unit)
+
+    @Test
+    fun day12() = Day12().check(Unit, Unit)
+
+    @Test
+    fun day13() = Day13().check(Unit, Unit)
+
+    @Test
+    fun day14() = Day14().check(Unit, Unit)
+
+    @Test
+    fun day15() = Day15().check(Unit, Unit)
+
+    @Test
+    fun day16() = Day16().check(Unit, Unit)
+
+    @Test
+    fun day17() = Day17().check(Unit, Unit)
+
+    @Test
+    fun day18() = Day18().check(Unit, Unit)
+
+    @Test
+    fun day19() = Day19().check(Unit, Unit)
+
+    @Test
+    fun day20() = Day20().check(Unit, Unit)
+
+    @Test
+    fun day21() = Day21().check(Unit, Unit)
+
+    @Test
+    fun day22() = Day22().check(Unit, Unit)
+
+    @Test
+    fun day23() = Day23().check(Unit, Unit)
+
+    @Test
+    fun day24() = Day24().check(Unit, Unit)
+
+    @Test
+    fun day25() = Day25().check(Unit, Unit)
+}
