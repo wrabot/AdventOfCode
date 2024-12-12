cac982bcfa3a8ca6575e98463eb2087d40d5759e
Wilfrid Rabot <wilfrid.rabot@orange.com>
2024-12-12 14:13:51 +0100

    Upgrade kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
-    kotlin("plugin.serialization") version "2.0.20"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
 }
 
 repositories {
@@ -12,7 +12,7 @@ dependencies {
     // to test new CompetitiveTools
     //testImplementation("wrabot.competitive:CompetitiveTools") { version { branch = "main" } }
     testImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.2")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:2.0.20")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:2.1.0")
 }
 
 tasks.test {
