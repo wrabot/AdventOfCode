72bc97729234e3cc7fbbdca95d147491c8d38ede
Wilfrid Rabot <wilfrid.rabot@orange.com>
2024-12-19 06:45:13 +0100

    add year 2024 day 19

--- src/test/kotlin/aoc2024/Day19.kt
+++ src/test/kotlin/aoc2024/Day19.kt
@@ -3,6 +3,20 @@ package aoc2024
 import Day
 
 class Day19(test: Int? = null) : Day(test) {
-    override fun solvePart1() = Unit
-    override fun solvePart2() = Unit
+    private val patterns = lines[0].split(", ")
+    private val designs = lines.drop(2)
+
+    override fun solvePart1() = designs.count { it.isPossibleWith() }
+
+    private val isPossibleWith = mutableMapOf<String, Boolean>()
+    private fun String.isPossibleWith(): Boolean = isEmpty() || isPossibleWith.getOrPut(this) {
+        patterns.any { startsWith(it) && removePrefix(it).isPossibleWith() }
+    }
+
+    override fun solvePart2() = designs.sumOf { it.ways() }
+
+    private val ways = mutableMapOf<String, Long>()
+    private fun String.ways(): Long = if (isEmpty()) 1 else ways.getOrPut(this) {
+        patterns.sumOf { if (startsWith(it)) removePrefix(it).ways() else 0 }
+    }
 }
--- src/test/kotlin/aoc2024/Year.kt
+++ src/test/kotlin/aoc2024/Year.kt
@@ -112,7 +112,10 @@ class Year2024 {
     }
 
     @Test
-    fun day19() = Day19().check(Unit, Unit)
+    fun day19() {
+        Day19(1).check(6, 16)
+        Day19().check(298, 572248688842069)
+    }
 
     @Test
     fun day20() = Day20().check(Unit, Unit)
